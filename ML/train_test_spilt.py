2344c4147a899aad62da7fc9254e6b970e55997d
HUGOKANG890308 <73536870+HUGOKANG890308@users.noreply.github.com>
2023-03-20 20:19:35 +0800

    git push

--- ML/train_test_spilt.py
+++ ML/train_test_spilt.py
@@ -2,18 +2,20 @@ import pandas
 import numpy as np
 from sklearn.model_selection import train_test_split
 class train_test_selection():
-    def __init__(self,dataset,seed,train_size=0.8):
+    def __init__(self,dataset,target,seed,train_size=0.8):
         '''        
         ;dataset= data which you want, type=DataFrame
+        ;target= target column, type=string
         ;seed,random seed, type=int
         ;if train=True   return:trainingdata
         ;if train=False  return:testingdata
         ;rtype: DataFrame
         '''
         self.dataset=dataset
+        self.target=target
         self.seed=seed
         self.train_size=train_size
-        self.x_train,self.x_test,self.y_train,self.y_test=train_test_split(self.dataset,test_size=1-self.train_size,random_state=self.seed)
+        self.x_train,self.x_test,self.y_train,self.y_test=train_test_split(self.dataset.drop([self.dataset[self.target]],axis=1),test_size=1-self.train_size,random_state=self.seed)
     
 '''
 def __getitem__(self):
