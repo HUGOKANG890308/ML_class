f1ae92825fdb4248b5fd422f6e1b8b3493569196
YangYiChen <yichenyang.mg09@nycu.edu.tw>
2023-05-10 19:13:37 +0800

    update code

--- 3.1_split.py
+++ 3.1_split.py
@@ -6,62 +6,21 @@ from sklearn.model_selection import StratifiedShuffleSplit
 
 
 df = pd.read_csv('data.csv')
-X = df.drop(['Bankrupt?'], axis=1)
+X = df.drop(['Bankrupt?'], axis = 1)
 Y = df['Bankrupt?']
 print(f'X is {X}, X.shape is {X.shape}\n\n')
 print(f'Y is {Y}, Y.shape is {Y.shape}\n\n')
 
-    
-def splitting_train_test(X, Y, our_test_size=0.2, our_random_state=0):
-    '''
-    input:
-        X = 資料 (非 target) ; type = pandas dataframe
-        Y = 資料 (target) ; type = pandas dataframe
-        our_test_size = size of training data ; type = float ; default = 0.2
-        our_random_state = random state ; type = int ; default = 0 
-
-    output:
-        x_train = training data of x ; type = pandas dataframe
-        x_test = testing data of x ; type = pandas dataframe
-        y_train = training data of y ; type = pandas dataframe
-        y_test = testing data of y ; type = pandas dataframe
-    '''
-    
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=our_test_size, random_state=our_random_state)
-    print(f'x_train is {x_train}, x_train.shape is {x_train.shape}\n\n')
-    print(f'x_test is {x_test}, x_test.shape is {x_test.shape}\n\n')
-    print(f'y_train is {y_train}, y_train.shape is {y_train.shape}\n\n')
-    print(f'y_test is {y_test}, y_test.shape is {y_test.shape}\n\n')
-    
-    return x_train, x_test, y_train, y_test
+#x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = our_test_size, random_state = our_random_state)
 
 
-def splitting_train_validation_HoldOut(X, Y, our_validation_size=0.2, our_random_state=0):
-    '''
-    input:
-        X = 資料 (非 target) ; type = pandas dataframe
-        Y = 資料 (target) ; type = pandas dataframe
-        our_validaiton_size = size of training data ; type = float ; default = 0.2
-        our_random_state = random state ; type = int ; default = 0 
-    
-    output:
-        x_train = training data of x ; type = pandas dataframe
-        x_validation = validation data of x ; type = pandas dataframe
-        y_train = training data of y ; type = pandas dataframe
-        y_validation = validation data of y ; type = pandas dataframe
-    '''
-    
-    x_train, x_validation, y_train, y_validation = train_test_split(X, Y, test_size=our_validation_size, random_state=our_random_state)
-    print(f'x_train is {x_train}, x_train.shape is {x_train.shape}\n\n')
-    print(f'x_validation is {x_validation}, x_validation.shape is {x_validation.shape}\n\n')
-    print(f'y_train is {y_train}, y_train.shape is {y_train.shape}\n\n')
-    print(f'y_validation is {y_validation}, y_validation.shape is {y_validation.shape}\n\n')
-    
-    return x_train, x_validation, y_train, y_validation
+#x_train, x_validation, y_train, y_validation = train_test_split(x_train, y_train, test_size = our_validation_size, random_state = our_random_state)
 
 
-def splitting_train_validation_other_methods(X, Y, method, n, our_random_state=None, our_shuffle=False):
+
+def splitting_train_validation_StratifiedKFold(X, Y, n, our_random_state = None, our_shuffle = False):
     '''
+    切出stratifiedkfold的train validation data
     input:
         X = 資料 (非 target) ; type = pandas dataframe
         Y = 資料 (target) ; type = pandas dataframe
@@ -79,43 +38,23 @@ def splitting_train_validation_other_methods(X, Y, method, n, our_random_state=N
         y_train = training data of y ; type = pandas dataframe
         y_validation = validation data of y ; type = pandas dataframe
     '''   
+   #可以用在unbalanced data上 因為在分的時候會讓unbalanced data平均分在每個fold裡
+   StratifiedKFold_result = StratifiedKFold(n_splits = n, random_state = our_random_state, shuffle = our_shuffle)
     
-    if method == 'KFold':
-        KFold_result = KFold(n_splits=n, random_state=our_random_state, shuffle=our_shuffle)
-        
-        for train_index, validate_index in KFold_result.split(X, Y):
-            print("Train index:", train_index, "Test index:", validate_index)
-            
-            x_train_raw, x_validation_raw = X.iloc[train_index], X.iloc[validate_index]
-            y_train_raw, y_validation_raw = Y.iloc[train_index], Y.iloc[validate_index]      
+   for train_index, validate_index in StratifiedKFold_result.split(X, Y):
+       print("Train index:", train_index, "Test index:", validate_index)
+       x_train_raw, x_validation_raw = X.iloc[train_index], X.iloc[validate_index]
+       y_train_raw, y_validation_raw = Y.iloc[train_index], Y.iloc[validate_index]
     
-    elif method == 'StratifiedKFold':
-        StratifiedKFold_result = StratifiedKFold(n_splits=n, random_state=our_random_state, shuffle=our_shuffle)
-    
-        for train_index, validate_index in StratifiedKFold_result.split(X, Y):
-            print("Train index:", train_index, "Test index:", validate_index)
-            x_train_raw, x_validation_raw = X.iloc[train_index], X.iloc[validate_index]
-            y_train_raw, y_validation_raw = Y.iloc[train_index], Y.iloc[validate_index]
-        
-    elif method == 'LeaveOneOut':
-        LeaveOneOut_result = LeaveOneOut()
-        
-        for train_index, validate_index in LeaveOneOut_result.split(X, Y):
-            print("Train index:", train_index, "Test index:", validate_index)
-            x_train_raw, x_validation_raw = X.iloc[train_index], X.iloc[validate_index]
-            y_train_raw, y_validation_raw = Y.iloc[train_index], Y.iloc[validate_index]
-    else:
-        print('wrong input of method /n/n')
-        exit
-
     print('/n/n')
     x_train = x_train_raw.values
     x_validation = x_validation_raw.values
     y_train = y_train_raw.values
     y_validation = y_validation_raw.values
-    print(f'x_train is {x_train}, x_train.shape is {x_train.shape}\n\n')
-    print(f'x_validation is {x_validation}, x_validation.shape is {x_validation.shape}\n\n')
-    print(f'y_train is {y_train}, y_train.shape is {y_train.shape}\n\n')
-    print(f'y_validation is {y_validation}, y_validation.shape is {y_validation.shape}\n\n')
+    
+    #print(f'x_train is {x_train}, x_train.shape is {x_train.shape}\n\n')
+    #print(f'x_validation is {x_validation}, x_validation.shape is {x_validation.shape}\n\n')
+    #print(f'y_train is {y_train}, y_train.shape is {y_train.shape}\n\n')
+    #print(f'y_validation is {y_validation}, y_validation.shape is {y_validation.shape}\n\n')
         
     return x_train, x_validation, y_train, y_validation
--- 3.2_standardize.py
+++ 3.2_standardize.py
@@ -3,59 +3,44 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 
-def standardize(training_data, validation_data, testing_data, method):
+def standardize(x_training_data, x_validation_data, x_testing_data, method): 
     '''
+    標準化資料，只丟x進來
     input:
-        training_data = input training data(data after spiltting) ; type = pandas dataframe
-        validation_data = input validation data(data after spiltting) ; type = pandas dataframe
-        testing_data = input testing data(data after spiltting) ; type = pandas dataframe
+        x_training_data = input training data(data after spiltting) ; type = pandas dataframe
+        x_validation_data = input validation data(data after spiltting) ; type = pandas dataframe
+        x_testing_data = input testing data(data after spiltting) ; type = pandas dataframe
         method = input method you use to standardize data ; type = string
                 1. standardize the data
                 2. minmax the data
 
     output:
-        training_data = training data after standardize ; type = pandas dataframe
-        validation_data = validation data after standardize ; type = pandas dataframe
-        testing_data = testing data after standardize ; type = pandas dataframe
+        x_training_data = training data after standardize ; type = pandas dataframe
+        x_validation_data = validation data after standardize ; type = pandas dataframe
+        x_testing_data = testing data after standardize ; type = pandas dataframe
     '''
 
-    if method == 'z-score normalization':
+    if method == 'z_score_normalization':
+        temp = StandardScaler()
         
-        zscore_training  = StandardScaler().fit_transform(training_data)
-        zscore_validation  = StandardScaler().fit_transform(validation_data)
-        zscore_testing  = StandardScaler().fit_transform(testing_data)
-        print(zscore_training)
-        print(zscore_validation)
-        print(zscore_testing)
+        #training_data  = (zscore_training  + 3)/6
+        #validation_data  = (zscore_validation  + 3)/6
+        #testing_data  = (zscore_testing  + 3)/6
         
-        training_data  = (zscore_training  + 3)/6
-        validation_data  = (zscore_validation  + 3)/6
-        testing_data  = (zscore_testing  + 3)/6
-        
-    elif method == 'min max':
-        MinMaxScaler().feature_range = (0, 1) #這句是否需要？
-        
-        training_data  = StandardScaler().fit_transform(training_data)
-        validation_data  = StandardScaler().fit_transform(validation_data)
-        testing_data  = StandardScaler().fit_transform(testing_data)
+    elif method == 'min_max':
+        temp =  MinMaxScaler()
     else:
         print('wrong input of method /n/n')
-        exit        
-        
-    print(training_data)
-    print(validation_data)
-    print(testing_data)
+        exit      
+  
+    try:
+        x_training_data  = temp.fit_transform(x_training_data)
+        x_validation_data  = temp.transform(x_validation_data)
+        x_testing_data  = temp.transform(x_testing_data)
+    except:
+        print('please use right method /n/n')
     
-    return training_data, validation_data, testing_data
+    return x_training_data, x_validation_data, x_testing_data
     
-    '''
-    else:
-        scaler = MinMaxScaler()
-        scaler.fit(X)
-        X_scaled = scaler.transform(X)
-        X_scaled.min(axis=0)
-        X_scaled.max(axis=0)
-        X_manual_scaled = (X — X.min(axis=0)) / (X.max(axis=0) — X.min(axis=0))
-        print(np.allclose(X_scaled, X_manual_scaled)) # True
-    '''
+  
     
