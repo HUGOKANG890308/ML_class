a0928bc204b78f6888c18cad203224ec5bafe471
HUGOKANG890308 <73536870+HUGOKANG890308@users.noreply.github.com>
2023-05-20 04:39:08 +0800

    Update structure.py

--- structure.py
+++ structure.py
@@ -349,7 +349,6 @@ def evaluation(y_test, y_pred):
     f_beta = round(fbeta_score(y_test, y_pred, beta=3),4)
     
     return ac, f1, pre, rec, auc, f_beta
-
 def basic_ml(using_model , X_train, y_train, X_test, y_test ):
     '''
     to return evaluate dataframe
@@ -379,10 +378,11 @@ def basic_ml(using_model , X_train, y_train, X_test, y_test ):
 df = basic_ml(using_model={'xgb': XGBClassifier(), 'rf': RandomForestClassifier(
 )}, X_train, y_train, X_test, y_test)
 '''
-
-def objective(trial, method):
+    
+def objective(trial, method, X_train, y_train, X_val, y_val):
     '''
     method: input using model; type: string
+         can be one of the following: 'svm', 'rf', 'xgb'
     clf: input using model; type: sklearn model
     '''
     if method == 'svm':
@@ -390,7 +390,7 @@ def objective(trial, method):
         kernel = trial.suggest_categorical(
             'kernel', ['linear', 'poly', 'rbf', 'sigmoid'])
         degree = trial.suggest_int('degree', 2, 5)
-        clf=SVC(C=C, kernel=kernel, degree=degree)
+        clf=SVC(C=C, kernel=kernel, degree=degree,random_state=our_random_state)
 
     elif method == 'rf':
         max_depth = trial.suggest_int("max_depth", 2, 128)
@@ -399,7 +399,7 @@ def objective(trial, method):
         min_samples_leaf = int(trial.suggest_int('min_samples_leaf', 2, 128))
         criterion = trial.suggest_categorical("criterion", ["gini", "entropy"])
         clf=RandomForestClassifier(min_samples_split=min_samples_split,
-                max_leaf_nodes=max_leaf_nodes, criterion=criterion, random_state=Random_state, max_depth=max_depth,
+                max_leaf_nodes=max_leaf_nodes, criterion=criterion, random_state=our_random_state, max_depth=max_depth,
                 min_samples_leaf=min_samples_leaf)
     elif method == 'xgb':
         max_depth = trial.suggest_int("max_depth", 2, 128)
@@ -410,7 +410,8 @@ def objective(trial, method):
             'colsample_bytree', 0.5, 1, 0.1)
         learning_rate = trial.suggest_loguniform('learning_rate', 1e-5, 1e-2)
         clf=XGBClassifier(max_depth=max_depth, min_child_weight=min_child_weight, gamma=gamma, subsample=subsample,
-                colsample_bytree=colsample_bytree, learning_rate=learning_rate)
+                colsample_bytree=colsample_bytree, learning_rate=learning_rate,
+                random_state=our_random_state)#, tree_method='gpu_hist' if torch.cuda.is_available() else 'auto')
     else:
         raise ValueError(f"Invalid method '{method}'")
     clf.fit(X_train, y_train)
@@ -421,25 +422,39 @@ def objective(trial, method):
     '''
     return scores
 
-def study(method='svm', n_trials=10):
+def study(method, n_trials,X_train, y_train, X_val, y_val):
     '''
-    method : input using model; type: string, default: 'xgb'
+    method : input using model; type: string, 
+        can be one of the following: 'svm', 'rf', 'xgb'
     n_trials : input number of trials; type: int
+    X_train: input training data ; type: pandas dataframe
+    y_train: input training label ; type: pandas dataframe
+    X_val: input validation data ; type: pandas dataframe
+    y_val: input validation label ; type: pandas dataframe
+    
     '''
-    study = optuna.create_study()
-    study.optimize(lambda trial: objective(trial, method), n_trials=n_trials)
+    study = optuna.create_study(direction='maximize')
+    study.optimize(lambda trial: objective(trial, method,X_train, y_train, X_val, y_val), n_trials=n_trials)
     '''
     output: best params of model type: dictionary
     '''
     return study.best_params
 
-'''
-example of using study
-basic_ml(using_model={'xgb': XGBClassifier(**study(method='xgb', n_trials=10)),'xgb1':XGBClassifier()},
- X_train=pd.concat([X_train, X_val], axis=0), y_train=pd.concat([y_train, y_val], axis=0), 
- X_test=X_test, y_test=y_test)
+
 
 '''
+example of using study
+basic_ml(using_model={'xgb': XGBClassifier(**study(method='xgb', n_trials=10 ,X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val)), 
+                      'xgb1':XGBClassifier(random_state=our_random_state),
+                      'rf': RandomForestClassifier(**study(method='rf', n_trials=10 ,X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val)),
+                      'rf1':RandomForestClassifier(random_state=our_random_state),
+                      'svm': SVC(**study(method='svm', n_trials=10 ,X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val)),
+                      'svm1':SVC(random_state=our_random_state),
+                      },
+ X_train=pd.concat([X_train, X_val], axis=0), y_train=pd.concat([y_train, y_val], axis=0), 
+ X_test=X_test, y_test=y_test)
+'''
+
 def objective_nn(trial):
     
     '''
